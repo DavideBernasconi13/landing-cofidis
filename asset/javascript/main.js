16319ea8827297988ab2a43fcc27ea4e42ef1699
davidebernasconi13 <davideb.bernasconi@gmail.com>
2025-02-21 14:33:13 +0100

    marquee animation with Javascript

--- asset/javascript/main.js
+++ asset/javascript/main.js
@@ -21,3 +21,49 @@ function showHideText() {
     })
 }
 showHideText(); 
+
+
+/*-------------------------------------------
+Marquee animation 
+--------------------------------------------*/
+document.addEventListener('DOMContentLoaded', function () {
+
+    const duration = 12000; // durata dell'animazione in ms
+    const pauseTime = 2000; // tempo di pausa in ms
+    const directionAnimation = 'left';  // direzione dello scorrimento: left or right  
+
+    const marquee = document.querySelector('.marquee');
+    const span = marquee.querySelector('span');
+
+    const marqueeWidth = marquee.offsetWidth;
+    const spanWidth = span.offsetWidth;
+
+    let keyframes = [];
+    if ('left' === directionAnimation) {
+        keyframes = [
+            { transform: `translateX(${marqueeWidth}px)` },
+            { transform: `translateX(${-spanWidth}px)` }
+        ];
+    } else if ('right' === directionAnimation) {
+        keyframes = [
+            { transform: `translateX(${-spanWidth}px)` },
+            { transform: `translateX(${marqueeWidth}px)` }
+        ];
+    }
+
+    let options = {
+        duration: duration,
+        iterations: 2, // una sola iterazione per volta
+        easing: "linear"
+    };
+
+    function startAnimation() {
+        const marqueeAnimation = span.animate(keyframes, options);
+
+        marqueeAnimation.onfinish = function () {
+            setTimeout(startAnimation, pauseTime); // Dopo la pausa, riavvia l'animazione
+        };
+    }
+
+    startAnimation();
+});
